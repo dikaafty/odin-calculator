62530d90a2ed064c85111201efb861fb2d199946
Dika Afty <dikaafty@gmail.com>
2024-09-18 01:47:13 +0700

    Create Function For Appending Number

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ let currentInput = '';
 let operator = null;
 let firstOperand = null;
 
+function appendNumber(num) {
+  currentInput += num;
+  updateDisplay();
+}
+
 function updateDisplay() {
   document.getElementById('display').value = currentInput;
 }
