52cff434f168070b5691d04f44ef7c7fb7e949aa
Dika Afty <dikaafty@gmail.com>
2024-12-17 13:50:35 +0700

    Select Necessary Element

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-// Variables
+// Selection
+const display = document.querySelector(".display");
+const equalsBtn = document.querySelector(".equals-btn");
+const deleteBtn = document.querySelector('.delete-btn');
+const clearBtn = document.querySelector(".clear-btn");
 
+// Variables
 let firstOperand = "";
 let secondOperand = "";
 let operator = null;
